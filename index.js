b0b46b848aa51b30791672a9dbecd3f38c6b5b05
rohitsingh6200 <126147191+rohitsingh6200@users.noreply.github.com>
2023-03-13 08:33:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const getcolor = () =>
+{
+    //hex code
+    const randomNumber =Math.floor(Math.random() * 16777215);
+    console.log(randomNumber);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+    navigator.clipboard.writeText(randomCode); // copy code genator
+}
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+//intial call
+getcolor();
